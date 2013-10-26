4c7ce4904330d4a968b300edd0529ec5ffa71d74
KuoE0 <kuoe0.tw@gmail.com>
2013-10-27 04:37:56 +0800

    add title for setup page

--- setup.php
+++ setup.php
@@ -44,6 +44,7 @@ catch (PDOException $e) {
 
 <html>
 	<head>
+		<title>Setup for PresentReg</title>
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="css/style.css" rel="stylesheet">
 	</head>
