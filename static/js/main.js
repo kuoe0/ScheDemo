8f57541bb058e2f8a8551b073aed5b2a81f5da66
KuoE0 <kuoe0.tw@gmail.com>
2014-03-06 22:56:50 +0800

    link click event to setup button

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+/*
+ * main.js
+ * Copyright (C) 2014 KuoE0 <kuoe0.tw@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+$(function() {
+
+
+	$('#btn-setup').click(function () {
+		window.location.href="setup.php";
+	});
+
+});
+
