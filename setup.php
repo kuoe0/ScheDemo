6a67781d1a3032a8fd57433d75e47ece796ae6fb
KuoE0 <kuoe0.tw@gmail.com>
2014-03-07 03:13:06 +0800

    remove time/presenter template

--- setup.php
+++ setup.php
@@ -53,7 +53,6 @@ catch (PDOException $e) {
 		<link href="static/components/semantic/build/packaged/css/semantic.min.css" rel="stylesheet" />
 		<script src="static/components/semantic/build/packaged/javascript/semantic.min.js"></script>
 
-
 		<script src="static/js/main.js"></script>
 		<link href="static/css/style.css" rel="stylesheet" />
 	</head>
@@ -166,108 +165,28 @@ catch (PDOException $e) {
 						</div>
 					</div>
 
-					<div id="time-rule-list">
-						<div class="ui mini right floated blue button">
+					<div>
+						<div id="btn-add-time" class="ui mini right floated blue button">
 							<i class="add icon"></i>
-							Add
+							Time
 						</div>
 						<legend>Manual Time Importing</legend>
-						<div class="ui two column grid">
-							<div class="column">
-								<div class="ui raised segment">
-									<div class="ui right red corner label">
-										<i class="delete basic icon"></i>
-									</div>
-									<div class="two fields">
-										<div class="field">
-											<label>Start Date</label>
-											<div class="ui left icon input">
-												<input type="date" name="start-date" />
-												<i class="calendar icon"></i>
-											</div>
-										</div>
-										<div class="field">
-											<label>End Date</label>
-											<div class="ui left icon input">
-												<input type="date" name="end-date" />
-												<i class="calendar icon"></i>
-											</div>
-										</div>
-									</div>
-
-									<div class="two fields">
-										<div class="field">
-											<label>Start Time</label>
-											<div class="ui left icon input">
-												<input type="time" name="start-time" />
-												<i class="time icon"></i>
-											</div>
-										</div>
-										<div class="field">
-											<label>End Time</label>
-											<div class="ui left icon input">
-												<input type="time" name="end-time" />
-												<i class="time icon"></i>
-											</div>
-										</div>
-									</div>
-
-									<div class="two fields">
-										<div class="field">
-											<label># of People</label>
-											<div class="ui left icon input">
-												<input type="number" name="quota" />
-												<i class="users icon"></i>
-											</div>
-										</div>
-										<div class="field">
-											<label>Repeat</label>
-											<select name="repeat-mode">
-												<option value="none">None</option>
-												<option value="daily">Daily</option>
-												<option value="weekly">Weekly</option>
-												<option value="monthly">Monthly</option>
-											</select>
-										</div>
-									</div>
-								</div>
-							</div>
+						<div id="time-list" class="ui two column grid">
 						</div>
 					</div>
 
-					<div id="presenter-list">
-						<div class="ui mini right floated blue button">
+					<divid="presenter-list">
+						<div id="btn-add-presenter" class="ui mini right floated blue button">
 							<i class="add icon"></i>
-							Add
+							Presenter
 						</div>
 						<legend>Manual Presenter(s) Importing</legend>
-						<div class="ui two column grid">
-							<div class="column">
-								<div class="ui raised segment">
-									<div class="ui right red corner label">
-										<i class="delete basic icon"></i>
-									</div>
-									<div class="three fields">
-										<div class="field">
-											<label>ID</label>
-											<input type="text" name="presenter-id" />
-										</div>
-										<div class="field">
-											<label>Name</label>
-											<input type="text" name="presenter-name" />
-										</div>
-										<div class="field">
-											<label>Group</label>
-											<input type="text" name="presenter-group" />
-										</div>
-									</div>
-								</div>
-							</div>
+						<div id="presenter-list" class="ui two column grid">
 						</div>
 					</div>
 
-					<input type="hidden" name="time-rule-cnt" value=0/>
-					<input type="hidden" name="presenter-cnt" value=0/>
+					<input type="hidden" name="time-cnt" value="0" />
+					<input type="hidden" name="presenter-cnt" value="0" />
 
 					<div class="ui divider"></div>
 
@@ -282,35 +201,6 @@ catch (PDOException $e) {
 				<p>Powered by <a href="http://kuoe0.tw/">KuoE0</a>.</p>
 			</div>
 		</div>
-
-		<script>
-			var rule_cnt = 0;
-
-			function add_rule() {
-
-				datetime_rule = jQuery('<div class="datetime-rule" id="datetime-' + rule_cnt + '"><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;<input type="date" name="begin-date-' + rule_cnt + '" /> ~ <input type="date" name="end-date-' + rule_cnt + '" disabled /><span class="glyphicon glyphicon-refresh"></span>&nbsp;&nbsp;<select name="repeat-' + rule_cnt + '" onchange="EndDateStatus(' + rule_cnt + ', this)" ><option value="none">none</option><option value="daily">daily</option><option value="weekly">weekly</option><option value="monthly">monthly</option></select><span class="glyphicon glyphicon-time"></span> <input type="time" name="begin-time-' + rule_cnt + '" /> ~ <input type="time" name="end-time-' + rule_cnt + '" /> <span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;<input type="number" name="quota-' + rule_cnt + '" min="1" value="1" />&nbsp;&nbsp;<span class="glyphicon glyphicon-trash" onclick="remove_rule(' + rule_cnt + ')"></span></div>');
-
-				jQuery('#timeslot_rules').append(datetime_rule);
-				++rule_cnt;
-
-				$('input[name="rule-cnt"]').val(rule_cnt);
-			};
-
-			function remove_rule(id) {
-				jQuery('#datetime-' + id).remove();
-			}
-
-			function EndDateStatus(id, sel) {
-				if (sel.value == 'none') {
-					$('input[name="end-date-' + id + '"]').attr('disabled', 'disabled');
-				}
-				else {
-					$('input[name="end-date-' + id + '"]').removeAttr('disabled');
-				}
-			}
-
-			
-		</script>
 	</body>
 </html>
 
