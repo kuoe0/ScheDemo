7ce919cb11a45bb17805fbb4c874cd6732337d46
KuoE0 <kuoe0.tw@gmail.com>
2014-03-07 03:12:33 +0800

    dynamically add/delete presenter

--- static/js/main.js
+++ static/js/main.js
@@ -13,10 +13,26 @@ $(function() {
 
 		var time_result = '<div id="column-time-' + time_cnt + '" class="column"> <div class="ui raised segment"> <div id="btn-delete-time-' + time_cnt + '" class="ui right red corner label"> <i class="delete basic icon"></i> </div> <div class="two fields"> <div class="field"> <label>Start Date</label> <div class="ui left icon input"> <input type="date" name="start-date-' + time_cnt + '" /> <i class="calendar icon"></i> </div> </div> <div class="field"> <label>End Date</label> <div class="ui left icon input"> <input type="date" name="end-date-' + time_cnt + '" /> <i class="calendar icon"></i> </div> </div> </div> <div class="two fields"> <div class="field"> <label>Start Time</label> <div class="ui left icon input"> <input type="time" name="start-time-' + time_cnt + '" /> <i class="time icon"></i> </div> </div> <div class="field"> <label>End Time</label> <div class="ui left icon input"> <input type="time" name="end-time-' + time_cnt + '" /> <i class="time icon"></i> </div> </div> </div> <div class="two fields"> <div class="field"> <label># of People</label> <div class="ui left icon input"> <input type="number" name="quota-' + time_cnt + '" /> <i class="users icon"></i> </div> </div> <div class="field"> <label>Repeat</label> <select name="repeat-' + time_cnt + '"> <option value="none">None</option> <option value="daily">Daily</option> <option value="weekly">Weekly</option> <option value="monthly">Monthly</option> </select> </div> </div> </div> </div>';
 
-		$('#time-rule-list').append(time_result);
+		$('#time-list').append(time_result);
 
 		$('#btn-delete-time-' + time_cnt).click(function () {
 			$('#column-time-' + time_cnt).remove();
+		});
+
+	}
+
+	function add_presenter() {
+		var presenter_cnt = $('input[name="presenter-cnt"]').val();
+		$('input[name="presenter-cnt"]').val(parseInt(presenter_cnt) + 1);
+
+		var presenter_result = '<div id="column-presenter-' + presenter_cnt + '" class="column"> <div class="ui raised segment"> <div id="btn-delete-presenter-' + presenter_cnt + '" class="ui right red corner label"> <i class="delete basic icon"></i> </div> <div class="three fields"> <div class="field"> <label>ID</label> <input type="text" name="presenter-id-' + presenter_cnt + '" /> </div> <div class="field"> <label>Name</label> <input type="text" name="presenter-name-' + presenter_cnt + '" /> </div> <div class="field"> <label>Group</label> <input type="text" name="presenter-group-' + presenter_cnt + '" /> </div> </div> </div> </div>';
+
+		console.log(presenter_result);
+
+		$('#presenter-list').append(presenter_result);
+
+		$('#btn-delete-presenter-' + presenter_cnt).click(function () {
+			$('#column-presenter-' + presenter_cnt).remove();
 		});
 
 	}
@@ -34,6 +50,7 @@ $(function() {
 	});
 
 	$('#btn-add-time').click(add_time);
+	$('#btn-add-presenter').click(add_presenter);
 
 
 });
