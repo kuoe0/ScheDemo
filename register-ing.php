20f546382b7a351e65453d5a035ede35e1ce88e3
KuoE0 <kuoe0.tw@gmail.com>
2014-03-09 04:50:30 +0800

    register worked

--- register-ing.php
+++ register-ing.php
@@ -16,58 +16,27 @@ include_once 'db_con.php';
 include_once 'global.php';
 include_once 'function.php';
 
-$group_id = $_POST['group_id'];
-$time_id = $_POST['time_id'];
+$group_id = $_POST['group-id'];
+$time_id = $_POST['time-id'];
 $title = $_POST['title'];
 
-$current = new DateTime(date('Y-m-d H:i', time()));
-$begin_opening = new DateTime(date('Y-m-d H:i', time()));
-$end_opening = new DateTime(date('Y-m-d H:i', time()));
-
-$sql = "SELECT `value` FROM `attributes` WHERE `attr` = 'begin-opening'";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$data_row = $stmt->fetch();
-
-if ($data_row) {
-	$begin_opening = new DateTime($data_row['value']);
+$current_time = new DateTime(date('Y-m-d H:i', time()));
+$start_opening = get_attr($db, 'start_opening');
+if ($start_opening == '') {
+	$start_opening = new DateTime(date('Y-m-d H:i', time()));
+}
+$end_opening = get_attr($db, 'end_opening');
+if ($end_opening == '') {
+	$end_opening = new DateTime(date('Y-m-d H:i', time()));
 }
 
-$sql = "SELECT `value` FROM `attributes` WHERE `attr` = 'end-opening'";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$data_row = $stmt->fetch();
-
-if ($data_row) {
-	$end_opening = new DateTime($data_row['value']);
-}
-
-$sql = "SELECT `value` FROM `attributes` WHERE `attr` = 'url'";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$url = $stmt->fetch()['value'];
-
-if ($current < $begin_opening) {
-	echo '<h2>The system will open at ' . $begin_opening->format('Y-m-d H:i') . ' ~ ' . $end_opening->format('Y-m-d H:i') . '.</h2>';
-	echo '<p>Redirect after 5 sec...</p>';
-	header("Refresh: 5; URL=" . $url);
-	die;
-}
-elseif ($current > $end_opening) {
-	echo '<h2>The system had been closed.</h2>';
-	echo '<p>Redirect after 5 sec...</p>';
-	header("Refresh: 5; URL=" . $url);
+if ($current_time < $begin_opening) {
+	echo json_encode(array('status' => false, 'msg' => 'System will start at ' . $start_opening . '.'));
 	die;
 }
 
-$sql = "SELECT DISTINCT `registered` FROM `presenters` WHERE `group_id` = :group_id";
-$stmt = $db->prepare($sql);
-$stmt->execute(array(':group_id' => $group_id));
-
-if ($stmt->fetch()['registered'] == '1') {
-	echo '<h2>The presenters have registered. Please try again...</h2>';
-	echo '<p>Redirect after 5 sec...</p>';
-	header("Refresh: 5; URL=" . $url);
+if ($current_time > $end_opening) {
+	echo json_encode(array('status' => false, 'msg' => 'System has been closed at ' . $end_opening . '.'));
 	die;
 }
 
@@ -76,29 +45,28 @@ $stmt = $db->prepare($sql);
 $stmt->execute(array(':time_id' => $time_id));
 
 if ($stmt->fetch()['occupied'] == '1') {
-	echo '<h2>The time has been occupied. Please try again...</h2>';
-	echo '<p>Redirect after 5 sec...</p>';
-	header("Refresh: 5; URL=" . $url);
+	echo json_encode(array('status' => false, 'msg' => 'This time has been chosen!'));
+	die;
+}
+
+$sql = "SELECT DISTINCT `registered` FROM `groups` WHERE `group_id` = :group_id";
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':group_id' => $group_id));
+
+if ($stmt->fetch()['registered'] == '1') {
+	echo json_encode(array('status' => false, 'msg' => 'This presenter has been registered!'));
 	die;
 }
 
 # generate random password
 $passwd = gen_random_password();
-echo "<h2>Your password: $passwd</h2>";
-echo "<p>Please remember it! Go back to view <a href='$url'>result</a>.</p>";
 
+if (add_presentation($db, $title, $time_id, $group_id, $passwd)) {
+	echo json_encode(array('status' => true, 'password' => $passwd));
+	die;
+}
 
-$sql = "INSERT INTO `presentations` (`title`, `group_id`, `time_id`, `reg_time`) VALUES (:title, :group_id, :time_id, datetime('now'))";
-$stmt = $db->prepare($sql);
-$stmt->execute(array(':title' => $title, ':group_id' => $group_id, ':time_id' => $time_id));
-
-$sql = "UPDATE `timeslots` SET `occupied` = '1' WHERE `time_id` = :time_id";
-$stmt = $db->prepare($sql);
-$stmt->execute(array(':time_id' => $time_id));
-
-$sql = "UPDATE `presenters` SET `registered` = '1', `password` = :password WHERE `group_id` = :group_id";
-$stmt = $db->prepare($sql);
-$stmt->execute(array(':group_id' => $group_id, ':password' => sha1($passwd . $group_id)));
+echo json_encode(array('status' => false));
 
 ?>
 
